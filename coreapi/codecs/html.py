41ddd7a64464782e255b6ee8f8e01ab8e3854246
Tom Christie <tom@tomchristie.com>
2016-02-05 10:41:44 +0000

    HTML2Text in HTMLCodec

--- coreapi/codecs/html.py
+++ coreapi/codecs/html.py
@@ -1,9 +1,20 @@
 # coding: utf-8
 from coreapi.codecs.base import BaseCodec
+import html2text
+import re
 
 
 class HTMLCodec(BaseCodec):
     media_type = 'text/html'
 
     def load(self, bytes, base_url=None):
-        return bytes.decode('utf-8')
+        content = bytes.decode('utf-8')
+        # HTML to text.
+        converter= html2text.HTML2Text()
+        converter.ignore_links = True
+        content = converter.handle(content).strip()
+        # Strip leading/trailing whitespace in lines.
+        content = '\n'.join([line.strip() for line in content.splitlines()])
+        # Remove multiple newlines
+        content = re.sub(r'\n\n+', '\n', content)
+        return content
