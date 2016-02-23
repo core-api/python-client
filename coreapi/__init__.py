5a664cea982fc761e39de2766abf30c988883c39
Tom Christie <tom@tomchristie.com>
2016-02-23 13:49:59 +0000

    Version bump

--- coreapi/__init__.py
+++ coreapi/__init__.py
@@ -6,7 +6,7 @@ from coreapi.exceptions import ParseError, TransportError, ErrorMessage
 from coreapi import codecs, history, transports
 
 
-__version__ = '1.17.0'
+__version__ = '1.18.0'
 __all__ = [
     'Array', 'Document', 'Link', 'Object', 'Error', 'Field',
     'ParseError', 'NotAcceptable', 'TransportError', 'ErrorMessage',
