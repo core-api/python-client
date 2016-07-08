f8a7d6685b650fc09ab734fa6fd28ff5351f8891
Tom Christie <tom@tomchristie.com>
2016-07-08 12:21:00 +0100

    Version bump

--- coreapi/__init__.py
+++ coreapi/__init__.py
@@ -5,7 +5,7 @@ from coreapi.document import Array, Document, Link, Object, Error, Field
 from coreapi import codecs, transports
 
 
-__version__ = '1.29.0'
+__version__ = '1.30.0'
 __all__ = [
     'Array', 'Document', 'Link', 'Object', 'Error', 'Field',
     'Client',
