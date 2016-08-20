21b1c79b8f03612b73e1bf412e8962c721744cfd
Tom Christie <tom@tomchristie.com>
2016-08-20 14:54:23 +0100

    Seperate files and data

--- coreapi/__init__.py
+++ coreapi/__init__.py
@@ -4,7 +4,7 @@ from coreapi.client import Client
 from coreapi.document import Array, Document, Link, Object, Error, Field
 
 
-__version__ = '1.32.1'
+__version__ = '1.32.2'
 __all__ = [
     'Array', 'Document', 'Link', 'Object', 'Error', 'Field',
     'Client',
